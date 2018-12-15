4c04f150e110fe361f96517b075ea22434f20623
binidj <38330384+binidj@users.noreply.github.com>
2018-12-15 21:23:09 -0200

    Add files via upload

---
+++ PROJETO-CPU/controler.v
@@ -0,0 +1,69 @@
+module controler(clock, Tx, Ty, Tz, Tula, opcode);
+	
+	input wire [3:0] opcode;
+	input wire clock;
+	output reg [3:0] Tx;
+	output reg [3:0] Ty;
+	output reg [3:0] Tz;
+	output reg Tula;
+	
+	parameter Clear_Add = 4'b0000;
+	parameter Add_Load = 4'b0001;
+	parameter Add = 4'b0010;
+	parameter Shift_Right = 4'b0011;
+	parameter Disp = 4'b0100;
+			
+	always @(posedge clock) begin
+		
+		case (opcode)
+			
+			Clear_Add: begin
+					
+						Tx <= 4'b0001; // Load
+						Ty <= 4'b0010; // Clear
+						Tz <= 4'b0010; // Clear
+						Tula <= 1'b1; // X
+						
+					end	
+					
+			Add_Load: begin
+					
+						Tx <= 4'b0001; // Load
+						Ty <= 4'b0001; // Load
+						Tz <= 4'b0000; // Hold
+						Tula <= 1'b0; // Add
+						
+					end
+					
+			Add: begin
+					
+						Tx <= 4'b0000; // Hold
+						Ty <= 4'b0001; // Load
+						Tz <= 4'b0000; // Hold
+						Tula <= 1'b0; // Add
+						
+					end
+					
+			Shift_Right: begin
+					
+						Tx <= 4'b0000; // Hold
+						Ty <= 4'b0011; // Shift_Right
+						Tz <= 4'b0000; // Hold
+						Tula <= 1'b1; // X
+						
+					end
+					
+			Disp: begin
+					
+						Tx <= 4'b0010; // Clear
+						Ty <= 4'b0010; // Clear
+						Tz <= 4'b0001; // Load
+						Tula <= 1'b0; // X
+						
+					end
+		
+		endcase
+	
+	end
+	
+endmodule
---
+++ PROJETO-CPU/memory.v
@@ -0,0 +1,50 @@
+module memory (clock, pc, opcode, data, A, B);
+	
+	input wire [3:0] A;
+	input wire [3:0] B;
+	input wire [3:0] pc;
+	input wire clock;
+	output reg [3:0] opcode;
+	output reg [3:0] data;
+	
+	always @(posedge clock) begin
+		
+		case (pc)
+			
+			4'b0000 : begin //Clear_Load
+					
+						opcode <= 4'b0000;
+						
+					end
+					
+			4'b0001 : begin //Add_Load
+					
+						opcode <= 4'b0001;
+						data <= A;
+						
+					end
+					
+			4'b0010 : begin //Add
+					
+						opcode <= 4'b0010;
+						data <= B;
+						
+					end
+					
+			4'b0011 : begin //Shift_Right
+					
+						opcode <= 4'b0011;
+						
+					end
+					
+			4'b0100 : begin //Disp
+					
+						opcode <= 4'b0100;
+						
+					end
+			
+		endcase
+	
+	end
+			
+endmodule		
